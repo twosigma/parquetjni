6c25df8cc86db33e7f8059b87735b049f8a1bfc8
David Li <li.davidm96@gmail.com>
2020-04-27 15:49:49 -0400

    Add basic unit test

---
+++ src/test/java/com/twosigma/parquetjni/ParquetJniReaderTest.java
@@ -0,0 +1,53 @@
+/*
+ * Copyright 2020 Two Sigma Investments, LP.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.twosigma.parquetjni;
+
+import org.apache.arrow.memory.BufferAllocator;
+import org.apache.arrow.memory.RootAllocator;
+import org.apache.arrow.vector.ipc.message.ArrowRecordBatch;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+/** A simple integration test. Expects to run against a properly configured Minio. */
+class ParquetJniReaderTest {
+
+    private static final String TOKEN = "";
+    private static final String BUCKET = "bucket";
+    private static final String[] KEYS = {
+        "file_0.parquet",
+        "file_1.parquet",
+        "file_2.parquet",
+        "file_3.parquet",
+    };
+
+    @Test
+    public void testRead() throws Exception {
+        try (final BufferAllocator allocator = new RootAllocator(Integer.MAX_VALUE)) {
+            try (final ParquetJniReader file =
+                    ParquetJniReader.open(allocator, "http://localhost:9000", TOKEN, BUCKET, KEYS)) {
+                final ReadOptions readOptions = ReadOptions.newBuilder().build();
+                try (final ParquetJniReader.ArrowRecordBatchReader reader = file.readAll(readOptions)) {
+                    Assertions.assertTrue(reader.hasNext());
+                    while (reader.hasNext()) {
+                        try (final ArrowRecordBatch batch = reader.next()) {
+                            ;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
